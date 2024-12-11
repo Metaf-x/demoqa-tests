e9e4fded49dfc8b00a78f5a41009f36b96eaeceb
Olga Riabenko <me@oriabenko.ru>
2024-12-11 14:16:12 +0300

    initial commit

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -9,7 +9,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 10000;
+//        Configuration.timeout = 10000;
         Configuration.pageLoadStrategy = "eager";
     }
 
