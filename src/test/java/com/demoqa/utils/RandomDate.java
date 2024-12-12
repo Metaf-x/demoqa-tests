376b1746f6daa2b8fc6d4d42fc5545dcb8f60911
Olga Riabenko <me@oriabenko.ru>
2024-12-12 12:36:01 +0300

    add documentation to RandomDate class

--- src/test/java/com/demoqa/utils/RandomDate.java
+++ src/test/java/com/demoqa/utils/RandomDate.java
@@ -14,11 +14,22 @@ public class RandomDate {
     private LocalDate start;
     private LocalDate end;
 
+    /**
+     * Конструктор для генерации случайной даты, начиная с указанной даты до текущей даты.
+     *
+     * @param start начальная дата
+     */
     public RandomDate(LocalDate start) {
         this.start = start;
         this.date = this.getDate(this.start);
     }
 
+    /**
+     * Конструктор для генерации случайной даты в указанном диапазоне.
+     *
+     * @param start начальная дата
+     * @param end конечная дата
+     */
     public RandomDate(LocalDate start, LocalDate end) {
         if (start.isAfter(end)) {
             throw new IllegalArgumentException("Start date must be before end date");
@@ -28,6 +39,12 @@ public class RandomDate {
         this.date = this.getDate(this.start, this.end);
     }
 
+    /**
+     * Генерация случайной даты, начиная с указанной даты до текущей даты.
+     *
+     * @param startInclusive начальная дата
+     * @return случайная дата
+     */
     public LocalDate getDate(LocalDate startInclusive) {
         long startEpochDay = startInclusive.toEpochDay();
         long endEpochDay = LocalDate.now().toEpochDay();
@@ -38,6 +55,13 @@ public class RandomDate {
         return LocalDate.ofEpochDay(randomDay);
     }
 
+    /**
+     * Генерация случайной даты в указанном диапазоне.
+     *
+     * @param startInclusive начальная дата
+     * @param endExclusive конечная дата
+     * @return случайная дата
+     */
     public LocalDate getDate(LocalDate startInclusive, LocalDate endExclusive) {
         long startEpochDay = startInclusive.toEpochDay();
         long endEpochDay = endExclusive.toEpochDay();
@@ -48,14 +72,29 @@ public class RandomDate {
         return LocalDate.ofEpochDay(randomDay);
     }
 
+    /**
+     * Получение дня в формате "dd".
+     *
+     * @return день в формате "dd"
+     */
     public String getFormattedDay() {
         return this.date.format(DAY_FORMATTER);
     }
 
+    /**
+     * Получение месяца в формате "MMMM".
+     *
+     * @return месяц в формате "MMMM"
+     */
     public String getFormattedMonth() {
         return this.date.format(MONTH_FORMATTER);
     }
 
+    /**
+     * Получение года в формате "yyyy".
+     *
+     * @return год в формате "yyyy"
+     */
     public String getFormattedYear() {
         return this.date.format(YEAR_FORMATTER);
     }
